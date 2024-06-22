7a3c42fad111f189a1a5b30effdaf8990f33c8c3
Michael McGovern <mrmichaelmcgovern@gmail.com>
2024-06-22 18:33:40 +0200

    Update IEA scraper

--- IEA/iea_scraper.py
+++ IEA/iea_scraper.py
@@ -6,34 +6,122 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-URL = 'https://www.iea.org/data-and-statistics/data-tools/energy-statistics-data-browser'
-PRINT = True
+URL_DATA = 'https://www.iea.org/data-and-statistics/data-tools/energy-statistics-data-browser'
+URL_COUNTRIES = 'https://www.iea.org/countries'
+PRINT_DATA = True
+PRINT_COUNTRIES = True
 
 
-def get_iea(print_data=False):
+def open_iea_webpage_in_chrome(url) -> webdriver.Chrome:
     # Load webpage in Chrome
     service = Service(executable_path='IEA/chromedriver')
     driver = webdriver.Chrome(service=service)
-    driver.get(URL)
+    driver.get(url)
+    return driver
 
-    
-    # Select "Browse as tables"
+
+def open_iea_data_browser_tables() -> webdriver.Chrome:
+    # Open data browser webpage
+    driver = open_iea_webpage_in_chrome(URL_DATA)
+
+    # Select "Browse as tables" button
     WebDriverWait(driver, 3).until(
         EC.presence_of_element_located((By.XPATH, "//a[@data-tab-id='tables']"))
     )
     element = driver.find_element(By.XPATH, "//a[@data-tab-id='tables']")
     element.click()
 
-
-    # Select data options (fuel, country, year)
-    # TODO
-    ...
-
-
-    # Get table data
     WebDriverWait(driver, 3).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "div.m-data-table > table"))
     )
+
+    return driver
+
+
+def get_iea_countries(print_countries: bool) -> list:
+    try:
+        countries = pd.read_csv('IEA/iea_countries.csv')
+        countries = countries['Country'].tolist()
+
+    except FileNotFoundError:
+        driver = open_iea_webpage_in_chrome(URL_COUNTRIES)
+        WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div#countries h6 span"))
+        )
+        countries = [element.text for element in driver.find_elements(By.CSS_SELECTOR, "div#countries h6 span")]
+        driver.quit()
+
+        if print_countries:
+            countries = pd.Series(countries, name='Country').to_csv('IEA/iea_countries.csv', index=None)
+
+    return countries
+
+
+def get_iea_fuels(print_fuels: bool) -> list:
+    try:
+        fuels = pd.read_csv('IEA/iea_fuels.csv')
+        fuels = fuels['Fuel'].tolist()
+
+    except FileNotFoundError:
+        driver = open_iea_data_browser_tables()
+
+        # Get fuel list
+        fuel_select = driver.find_elements(By.CSS_SELECTOR, "select")[2]
+        fuel_options = fuel_select.find_elements(By.CSS_SELECTOR, 'option')
+        fuels = [option.get_attribute('innerHTML').strip() for option in fuel_options]
+
+        driver.quit()
+
+        if print_fuels:
+            fuels = pd.Series(fuels, name='Fuel').to_csv('IEA/iea_fuels.csv', index=None)
+
+    return fuels
+
+
+def get_iea_years(print_years: bool) -> list:
+    try:
+        years = pd.read_csv('IEA/iea_years.csv')
+        years = years['Year'].tolist()
+
+    except FileNotFoundError:
+        driver = open_iea_data_browser_tables()
+
+        # Get year list
+        year_select = driver.find_elements(By.CSS_SELECTOR, "select")[3]
+        year_options = year_select.find_elements(By.CSS_SELECTOR, 'option')
+        years = [option.get_attribute('innerHTML').strip() for option in year_options]
+
+        driver.quit()
+
+        if print_years:
+            years = pd.Series(years, name='Year').to_csv('IEA/iea_years.csv', index=None)
+
+    return years
+
+
+def get_iea_data(fuel:   list[str]=None, 
+                 country:list[str]=None, 
+                 year:   list[str]=None, 
+                 print_data:bool=False) -> pd.DataFrame:
+    # Open webpage
+    driver = open_iea_data_browser_tables()
+    
+    # Select data options (fuel, country, year)
+    # Fuel
+    # TODO: select fuel
+    if fuel:
+        ...
+    
+    # TODO: select country
+    if country:
+        ...
+    
+    # TODO: select year
+    if year:
+        ...
+
+
+    # Get table data
     table = driver.find_element(By.CSS_SELECTOR, "div.m-data-table > table").get_attribute("outerHTML")
     df = pd.read_html(table)[0]
 
@@ -42,7 +130,7 @@ def get_iea(print_data=False):
     driver.quit()
 
 
-    # Process dataset
+    # Clean dataset
     df = df.rename(columns={'Unnamed: 0': ''})
     df = df.set_index('')
     df = df.iloc[1:]
@@ -61,4 +149,7 @@ def get_iea(print_data=False):
 
 
 if __name__ == '__main__':
-    iea_data = get_iea(print_data=PRINT)
+    iea_countries = get_iea_countries(print_countries=PRINT_COUNTRIES)    
+    iea_fuels = get_iea_fuels(print_fuels=True)
+    iea_years = get_iea_years(print_years=True)    
+    iea_data  = get_iea_data(print_data=PRINT_DATA)
